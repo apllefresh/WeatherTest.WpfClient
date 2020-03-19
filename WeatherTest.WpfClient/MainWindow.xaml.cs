d6af725e1c17e7b165d1e29905c957a0082babf4
Константин Алексеев <arrogant93@mail.ru>
2020-03-20 02:55:38 +0300

    Add project files.

---
+++ WeatherTest.WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Windows;
+using WeatherTest.WpfClient.WeatherServiceReference;
+
+namespace WeatherTest.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly WeatherServiceClient _client; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _client = new WeatherServiceClient();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var t = _client.GetCities();
+            tb.Text = string.Join("\n", t.Select(s => s.Name).ToList());
+        }
+    }
+}
